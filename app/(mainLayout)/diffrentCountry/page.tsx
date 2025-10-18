715a2e0c5ab2e7595f3c22fe1ddf6389986e8047
Rasel Miah <rasel6041@gmail.com>
2025-10-18 17:52:01 +0100

    diffrentcountry  page news

--- app/(mainLayout)/diffrentCountry/page.tsx
+++ app/(mainLayout)/diffrentCountry/page.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 // components/general/countriesList.ts
-export const euCountries = [
+const euCountries = [
   { name: "Austria", flag: "/flags/Austria.png" },
   {
     name: "Bangladesh",
