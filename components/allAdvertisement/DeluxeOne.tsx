d92d9006da026a68a2168209d413a4f534d27a16
Rasel Miah <rasel6041@gmail.com>
2025-10-20 23:30:07 +0100

    size one advertise  delux 1

--- components/allAdvertisement/DeluxeOne.tsx
+++ components/allAdvertisement/DeluxeOne.tsx
@@ -51,7 +51,7 @@ export async function DeluxeOneAdvertise() {
               href="/about/advertise"
               className="inline-block text-white bg-primary hover:bg-primary/90 px-4 py-1.5 rounded-xs text-xs transition"
             >
-              Contact us for (DELUXE_1) Advertisement
+              Contact us for (DELUXE_1) ad.
             </Link>
           </div>
         </div>
