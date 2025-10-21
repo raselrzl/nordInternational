4a654d0fe11ca65aa74634558437697c992291d3
Rasel Miah <rasel6041@gmail.com>
2025-10-21 12:08:50 +0100

    add. on all

--- components/allAdvertisement/DeluxeTwo.tsx
+++ components/allAdvertisement/DeluxeTwo.tsx
@@ -40,7 +40,7 @@ export async function DeluxeTwoAdvertise() {
               <img
                 src={pro1.advertiseBanner}
                 alt={pro1.companyName}
-                className="w-[400px] h-[200px] md:h-[180px] rounded-xl object-cover"
+                className="w-[400px] h-[200px] md:h-[180px] rounded-xl object-fill"
               />
               <span className="absolute top-2 right-2 bg-primary text-white text-xs px-2 py-0.5 rounded">
                 Ad.
