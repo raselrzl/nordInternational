926f5760ea0cb3e01c396995455b7bd48e2639a5
Rasel Miah <rasel6041@gmail.com>
2025-10-23 08:59:20 +0100

    invoice reference number last eight digit of the id

--- app/(mainLayout)/layout.tsx
+++ app/(mainLayout)/layout.tsx
@@ -8,7 +8,7 @@ import NewsTicker from "@/components/general/NewsTicker";
 
 export default function MainLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="max-w-7xl mx-auto">
+    <div className="max-w-7xl mx-auto md:px-6 2xl:px-0">
       <div className="fixed top-0 left-0 right-0 z-50 bg-background shadow-md max-w-7xl mx-auto">
         <Navbar />
         <div className="flex justify-between items-center py-1 bg-primary-foreground">
