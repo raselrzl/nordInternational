f4cb21655ba95b9eaffaac0a34a8281b30208a75
Rasel Miah <rasel6041@gmail.com>
2025-10-24 11:22:28 +0100

    nabvar responsive

--- app/(adminLayout)/post-an-article/layout.tsx
+++ app/(adminLayout)/post-an-article/layout.tsx
@@ -42,7 +42,7 @@ export default async function AdminLayout({ children }: AdminLayoutProps) {
         </h1>
         </div>
         <div className="mt-34"></div>
-        <div className="p-2 md:px-4 border-1 mb-10 shadow">{children}</div>
+        <div className="p-2 md:px-4 border-1 mb-10 shadow min-h-[538px]">{children}</div>
       </div>
     </div>
   );
