9d81f4c93711889d970c0802b7f1965364cc7730
raselrzl <rasel6041@gmail.com>
2025-11-05 02:47:09 +0100

    logo changed

--- app/(mainLayout)/newsDetails/[articleId]/page.tsx
+++ app/(mainLayout)/newsDetails/[articleId]/page.tsx
@@ -147,7 +147,7 @@ export default async function NewsDetailsPage({ params }: { params: Params }) {
         <div className="flex flex-col font-bold mb-1 text-xl">
           <div className="flex flex-row pl-2 items-center">
             <User2 className="size-5 mr-1" />
-            <p>NORD Reporter</p>
+            <p>GLOBAL EYE Reporter</p>
           </div>
           <div className="flex flex-row  pl-2 items-center">
             <Clock className="size-5 mr-1" />
