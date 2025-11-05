05669260563bf0b0a79e06160918e9435368e1e6
raselrzl <rasel6041@gmail.com>
2025-11-05 22:28:39 +0100

    war session

--- components/general/LatestOpinions.tsx
+++ components/general/LatestOpinions.tsx
@@ -34,7 +34,7 @@ export default async function LatestOpinions() {
   return (
     <section className="md:rounded-xs dark:bg-gray-600">
       {/* Section Header */}
-      <div className="flex items-center justify-between mb-2 bg-amber-800 px-4 md:rounded-xl py-1">
+      <div className="flex items-center justify-between mb-2 px-4 md:rounded-xl py-1">
         <h1 className="font-bold py-1 flex"> <Newspaper className="mr-4"/> OPINIONS</h1>
         <ChevronRight className="block md:hidden" />
       </div>
