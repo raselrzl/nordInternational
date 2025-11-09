a0108f35d54d85a133ca664ac4908b251e917bdf
raselrzl <rasel6041@gmail.com>
2025-11-09 10:39:58 +0100

    changed homepage image

--- components/general/Footer.tsx
+++ components/general/Footer.tsx
@@ -115,14 +115,14 @@ export default function Footer() {
             </p>
             <div className="flex flex-row gap-3">
               {" "}
-              <a
+           {/*    <a
                 href="https://www.facebook.com"
                 target="_blank"
                 rel="noopener noreferrer"
                 aria-label="Facebook"
               >
                 <Facebook className="w-10 h-10 bg-primary p-1 rounded-xl text-accent hover:text-green-600" />
-              </a>
+              </a> */}
               <a
                 href="https://www.instagram.com/globaleyepress"
                 target="_blank"
@@ -131,14 +131,14 @@ export default function Footer() {
               >
                 <Instagram className="w-10 h-10 bg-primary p-1 rounded-xl text-accent hover:text-pink-500" />
               </a>
-              <a
+           {/*    <a
                 href="https://www.youtube.com"
                 target="_blank"
                 rel="noopener noreferrer"
                 aria-label="YouTube"
               >
                 <Youtube className="w-10 h-10 bg-primary p-1 rounded-xl text-accent hover:text-red-600" />
-              </a>
+              </a> */}
             </div>
           </div>
         </div>
--- components/general/socialLink.tsx
+++ components/general/socialLink.tsx
@@ -14,14 +14,14 @@ import React from "react";
 const SocialLinks = () => {
   return (
     <div className="flex items-center  justify-center gap-4 mt-6 ">
-      <a
+    {/*   <a
         href="https://www.facebook.com"
         target="_blank"
         rel="noopener noreferrer"
         aria-label="Facebook"
       >
         <Facebook className="w-10 h-10 bg-primary p-1 rounded-xl text-accent hover:text-green-600" />
-      </a>
+      </a> */}
       <a
         href="https://www.instagram.com/globaleyepress"
         target="_blank"
@@ -30,14 +30,14 @@ const SocialLinks = () => {
       >
         <Instagram className="w-10 h-10 bg-primary p-1 rounded-xl text-accent hover:text-pink-500" />
       </a>
-      <a
+     {/*  <a
         href="https://www.youtube.com"
         target="_blank"
         rel="noopener noreferrer"
         aria-label="YouTube"
       >
         <Youtube className="w-10 h-10 bg-primary p-1 rounded-xl text-accent hover:text-red-600" />
-      </a>
+      </a> */}
     </div>
   );
 };
