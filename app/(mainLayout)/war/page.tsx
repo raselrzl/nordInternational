7903fcd6086103f945b9afa8c6f04ebe3345a50b
raselrzl <rasel6041@gmail.com>
2025-12-24 12:44:50 +0100

    updated nav bar

--- app/(mainLayout)/war/page.tsx
+++ app/(mainLayout)/war/page.tsx
@@ -17,9 +17,8 @@ export default async function War({ searchParams }: SearchParamsProps) {
 
   return (
     <div className="mt-10">
-        <div className="font-extrabold pl-2 mb-2 flex items-center">
-          <List className="h-5 w-5 mr-2" />
-          WAR update
+        <div className="font-extrabold pl-2 mb-4 text-xl flex items-center uppercase border-l-10 border-orange-600">
+           WAR updates
         </div>
       <div className="grid grid-cols-1 max-w-3xl mx-auto">
         
