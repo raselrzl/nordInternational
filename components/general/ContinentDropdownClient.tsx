9321d155145f80c63622e9f08356eaee2b439870
raselrzl <rasel6041@gmail.com>
2025-12-24 02:31:28 +0100

    only employee have access to his employee page

--- components/general/ContinentDropdownClient.tsx
+++ components/general/ContinentDropdownClient.tsx
@@ -84,7 +84,7 @@ export default function ContinentDropdownRow({ data }: Props) {
                     key={item.country}
                     href={`/diffrentCountry?country=${item.country}`}
                     onClick={() => setOpen(null)}
-                    className="block px-4 py-2 text-sm hover:bg-gray-200"
+                    className="block px-4 py-2 text-sm hover:bg-black"
                   >
                     {item.country}{" "}
                     <span className="text-[8px] text-gray-400">
