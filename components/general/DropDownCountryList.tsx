b67f532535c62a21dfca2af9ed1ffd3f2172210c
raselrzl <rasel6041@gmail.com>
2025-12-29 23:24:31 +0100

    time page design

--- components/general/DropDownCountryList.tsx
+++ components/general/DropDownCountryList.tsx
@@ -204,7 +204,7 @@ export default function DropDownCountryList() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <button className="flex text-sm items-center border-none shadow-none px-0">
-          <p className="font-bold text-md uppercase">globe</p>
+          <p className="font-bold text-md uppercase">world</p>
           <ChevronDown className="h-4 w-4" />
         </button>
       </DropdownMenuTrigger>
